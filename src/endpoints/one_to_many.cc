bf3219a3a32ebd749ef44e4e4bb5fd3b0663cdd1
Michael Kutzner <174690291+MichaelKutzner@users.noreply.github.com>
2026-04-15 07:31:41 +0200

    WIP: Implement first tests

--- src/endpoints/one_to_many.cc
+++ src/endpoints/one_to_many.cc
@@ -16,6 +16,7 @@
 #include "motis/endpoints/routing.h"
 #include "motis/gbfs/routing_data.h"
 #include "motis/osr/mode_to_profile.h"
+#include "motis/place.h"
 #include "motis/timetable/modes_to_clasz_mask.h"
 
 namespace motis::ep {
@@ -61,11 +62,16 @@ api::oneToMany_response one_to_many_direct(
           mode == api::ModeEnum::WALK,
       "mode {} not supported for one-to-many", fmt::streamed(mode));
 
+  auto const transform_location = [&](auto const& l) {
+    auto const p = to_place(l, "", std::nullopt);
+    return get_location(p);
+  };
   auto const profile = to_profile(mode, pedestrian_profile, elevation_costs);
   auto const paths =
-      osr::route(to_profile_parameters(profile, params), w, l, profile, one,
-                 many, max_direct_time, dir, max_matching_distance, nullptr,
-                 nullptr, elevations_, [&](auto&&) { return with_distance; });
+      osr::route(to_profile_parameters(profile, params), w, l, profile,
+                 transform_location(one), utl::to_vec(many, transform_location),
+                 max_direct_time, dir, max_matching_distance, nullptr, nullptr,
+                 elevations_, [&](auto&&) { return with_distance; });
 
   return utl::to_vec(paths, [&](std::optional<osr::path> const& p) {
     return p
